20feb1e49bad11d2889fa24f411b24566b8cc017
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2024-06-19 14:30:38 -0400

    filtros
    filtro AZ e ZA

--- helloword/src/app/componentes/Main.js
+++ helloword/src/app/componentes/Main.js
@@ -1,21 +1,49 @@
+// async/await: quando é utilizado o async e o await é voltado para server da aplicação
+"use client"
 import Image from "next/image";
 import styles from "../page.module.css";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
+export default function Home() {
   //quando é utilizado o async e o await é voltado para server da aplicação
-  const resposta = await fetch("https://fakestoreapi.com/products", {
-    next: {
-      revalidate: 1
-    }
-  });
 
-  const campi = await resposta.json();
+const [lisProduct, setProduct] = useState([])
+
+useEffect(() => {
+  const getProduct = async () =>{
+    const resposta = await fetch("https://fakestoreapi.com/products", {next: {revalidate: 1}});
+    const produto = await resposta.json();
+    setProduct(produto)
+
+  }
+  getProduct()
+},[])/* o " []" diz que será disparado apenas quando a lista estiver vazia */ 
+/* td vez q a minha lista for modificada ele vai buscar através do fecth a informação que será salva na lista */
+
+  const orderAz = ()=>{
+    const lisAux = [...lisProduct].sort((a,b) => a.title.localeCompare(b.title))
+    setProduct(lisAux)
+  }
+
+  const orderZa = ()=>{
+    const lisAux = [...lisProduct].sort((a,b) => b.title.localeCompare(a.title))
+    setProduct(lisAux)
+  }
+
   return (
+    <>
+    <div className={styles.filters}>
+      <div>
+        <button onClick={orderAz}>AZ</button>
+        <button onClick={orderZa}>ZA</button>
+      </div>
+    </div>
+
     <main>
       {
-        campi.map((produto) =>
+        lisProduct.map((produto) =>
           <div className={styles.produtos}>
-            <Image width={400} height={450} src={produto.image} />
+            <Image width={250} height={250} src={produto.image} />
             <p className={styles.titulo}>{produto.title}</p>
             <p className={styles.descricao}>{produto.description}</p>
 
@@ -28,5 +56,6 @@ export default async function Home() {
         )
       }
     </main>
+    </>
   )
 }
